e99bfa5555e4f0a3c235cdcec79f81751e45d883
drafffffff <i0.0ia@qq.com>
2022-05-20 04:46:36 +0800

    10STEEPER

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include "Arduino.h"
 #include "untils.h"
-#include"SoftwareSerial.h"
+#include "SoftwareSerial.h"
+#include "stepperPin.h"
+SoftwareSerial step1;
+SoftwareSerial step2;
+SoftwareSerial step3;
+SoftwareSerial step4;
+SoftwareSerial step5;
+SoftwareSerial step6;
+SoftwareSerial step7;
+SoftwareSerial step8;
+SoftwareSerial step9;
+SoftwareSerial step10;
 void setup()
 {
   Serial.begin(57600);
-  Serial2.begin(38400);
+  step1.begin(115200, SWSERIAL_8N1, STEPPER1_R, STEPPER1_T, false);
+  step2.begin(115200, SWSERIAL_8N1, STEPPER2_R, STEPPER2_T, false);
+  step3.begin(115200, SWSERIAL_8N1, STEPPER3_R, STEPPER3_T, false);
+  step4.begin(115200, SWSERIAL_8N1, STEPPER4_R, STEPPER4_T, false);
+  step5.begin(115200, SWSERIAL_8N1, STEPPER5_R, STEPPER5_T, false);
+  step6.begin(115200, SWSERIAL_8N1, STEPPER6_R, STEPPER6_T, false);
+  step7.begin(115200, SWSERIAL_8N1, STEPPER7_R, STEPPER7_T, false);
+  step8.begin(115200, SWSERIAL_8N1, STEPPER8_R, STEPPER8_T, false);
+  step9.begin(115200, SWSERIAL_8N1, STEPPER9_R, STEPPER9_T, false);
+  step10.begin(115200, SWSERIAL_8N1, STEPPER10_R, STEPPER10_T, false);
 }
 
 void loop()
@@ -20,22 +40,20 @@ void loop()
   //   Serial2.write(inByte);
   // }
 
-
   for (int i = 0; i < 360; i += 10)
   {
 
     angle2(0x02, i, 100, 0);
-    delay(100);  
+    delay(100);
   }
-    for (int i = 360; i > 0; i -= 10)
+  for (int i = 360; i > 0; i -= 10)
   {
 
     angle2(0x02, i, 100, 0);
-    delay(100);  
+    delay(100);
   }
   //     Serial.print("read_pos(0x02):");
   //   Serial.println(read_pos(0x02));
 
   // delay(400);
-  
 }
---
+++ src/stepperPin.h
@@ -0,0 +1,31 @@
+
+#define STEPPER1_R 32
+#define STEPPER1_T 33
+
+#define STEPPER2_R 25
+#define STEPPER2_T 26
+
+#define STEPPER3_R 27
+#define STEPPER3_T 14
+
+#define STEPPER4_R 12
+#define STEPPER4_T 13
+
+#define STEPPER5_R 23
+#define STEPPER5_T 22
+
+#define STEPPER6_R 21
+#define STEPPER6_T 19
+
+#define STEPPER7_R 18
+#define STEPPER7_T 5
+
+#define STEPPER8_R 17
+#define STEPPER8_T 16
+
+#define STEPPER9_R 4
+#define STEPPER9_T 2
+
+#define STEPPER10_R 8
+#define STEPPER10_T 7
+
